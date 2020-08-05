fe04a8cccd413c9252d493acb31f8c514484c252
m.postovoy <m.postovoy@tinkoff.ru>
2020-08-05 23:45:41 +0500

    init

---
+++ mposter/mposter.py
@@ -0,0 +1,2 @@
+def mposter():
+    return {'test': 'text'}
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+
+import setuptools
+
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mposter",
+    version="0.0.1",
+    author="M.Postovoy",
+    author_email="mihan.45rus@ya.ru",
+    description="Test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],,
+)
