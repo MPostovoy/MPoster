640fcbcbad590f04da517ac901313fdb27a2d729
m.postovoy <m.postovoy@tinkoff.ru>
2020-08-05 23:49:54 +0500

    no message

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],,
+    ],
 )
