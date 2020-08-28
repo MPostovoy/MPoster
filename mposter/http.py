23e5b85895deff91a55112c772c7a6ec6e7623ca
m.postovoy <m.postovoy@tinkoff.ru>
2020-08-28 19:07:41 +0500

    no message

--- mposter/http.py
+++ mposter/http.py
@@ -2,13 +2,13 @@ import random
 import requests
 
 
-def get_proxies(errors: int = -1, paid_errors: int = 5, paid_type: int = -1,
+def get_proxies(errors: int = -1, paid_errors: int = 5, paid_type: int = -1, paid_check_host: str = '',
                 paid_host: str = 'http://88.198.167.20:8083/proxies?', paid_max_proxies: int = 12054,
                 vpn: bool = False, vpn_errors: int = 7,
                 tor_min_proxies: int = 8120, tor_max_proxies: int = 8191) -> dict:
     try:
         if errors < paid_errors:
-            _proxies = requests.get(f'{paid_host}type={paid_type}', timeout=1).json()
+            _proxies = requests.get(f'{paid_host}type={paid_type}&host={paid_check_host}', timeout=1).json()
             return _proxies
         elif paid_errors <= errors < vpn_errors and vpn:
             return {'http': 'http://crawlers:Z3WqSbWQaKGxb9eTvEyYC2kS2W4DHwUM1FfTwikF@167.86.80.177:8889',
