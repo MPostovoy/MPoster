04198303f7ac0dec0aa3f1cbbb9871ef090a8d7b
m.postovoy <m.postovoy@tinkoff.ru>
2021-06-07 13:49:33 +0500

    no message

--- mposter/additional.py
+++ mposter/additional.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import platform
 
 
 def write_result(path: str, result, type_w: str = 'a', errors: int = 0):
@@ -20,6 +21,13 @@ def write_result(path: str, result, type_w: str = 'a', errors: int = 0):
 
 def update_monitoring(crawler: str, reply: str, status: int = 1, host: str = 'localhost'):
     try:
+        _node = platform.node()
+
+        if _node in 'vm-additional-services02p.tcsbank.ru':
+            host = 'localhost:8100'
+        elif _node in 'm1-crawlig-portal-1.tcsbank.ru':
+            host = 'localhost'
+
         requests.get(f'http://{host}/api/v1/crawlers/reply?'
                      f'crawler_name={crawler}&status={status}&reply={reply}', timeout=0.1)
     except Exception:
